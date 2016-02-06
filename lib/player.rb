cc00d7197565f01c15073e402c44b9cad7680cc4
Timothy Barmann <tbarmann92@gmail.com>
2016-02-06 09:53:10 -0500

    Moved score_roll method to player class, adjusted score_roll_test.rb to reflect this change

--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,7 @@
 
+require_relative 'dice_set'
+require 'pry'
+
 class Player
   attr_reader :round, :score
   attr_accessor :name
@@ -8,17 +11,42 @@ class Player
   end
 
   def set_score points
-  	@score = (@score > 0 || (points >= 300)) ? @score + points : 0
-  	@round += 1
+  	@score = (@score > 0 || (points >= 300)) ? @score + points : 0	
   end
 
 
   def play_turn
+  	@round += 1
   	if @name
   		puts @name + ": "
   	end
   	puts "Rolling..."
+  	d = DiceSet.new
+    d.roll(5)
+
   end
+
+  def score_roll(roll)
+    @roll = roll
+    @points = 0
+    (1..6).to_a.each do |n|
+      while (@roll.count n) >= 3 do
+        remove_triplet(n)
+        @points += (n * 100) + (n==1 ? 900 : 0) 
+      end
+    end
+    @points += ((@roll.count 1) * 100) + ((@roll.count 5) * 50) 
+    @roll.delete(1)
+    @roll.delete(5)
+    return :points => @points, :non_scoring_dice => @roll
+  end
+
+  def remove_triplet(num)
+    3.times do
+      @roll.delete_at(@roll.index(num) || @roll.length)
+    end
+  end
+
 
 end
  
--- test/score_roll_test.rb
+++ test/score_roll_test.rb
@@ -1,32 +1,26 @@
 gem 'minitest'
 require 'minitest/autorun'
  
-require_relative '../lib/game'
+require_relative '../lib/player'
 
-class GameTest < Minitest::Test
-  def test_game_initializes_with_a_number_of_players
-    g = Game.new(2)
-    result = g.score_roll([1,1,1,1,1])
-    assert_equal 1200, result[:score]
+class ScoreRollTest < Minitest::Test
+  def test_scoring_works_correctly
+    p = Player.new
+    result = p.score_roll([1,1,1,1,1])
+    assert_equal 1200, result[:points]
     assert_equal [], result[:non_scoring_dice]
 
-    result = g.score_roll([5,1,3,4,1])
-    assert_equal 250, result[:score]
+    result = p.score_roll([5,1,3,4,1])
+    assert_equal 250, result[:points]
     assert_equal [3,4], result[:non_scoring_dice]
  
-    result = g.score_roll([1,1,1,3,1])
-    assert_equal 1100, result[:score]
+    result = p.score_roll([1,1,1,3,1])
+    assert_equal 1100, result[:points]
     assert_equal [3], result[:non_scoring_dice]
 
-    result = g.score_roll([2,4,4,5,4])
-    assert_equal 450, result[:score]
+    result = p.score_roll([2,4,4,5,4])
+    assert_equal 450, result[:points]
     assert_equal [2], result[:non_scoring_dice]
   end
 
 end
-
-# Throw       Score
-# ---------   ------------------
-# 5 1 3 4 1   50 + 2 * 100 = 250
-# 1 1 1 3 1   1000 + 100 = 1100
-# 2 4 4 5 4   400 + 50 = 450
