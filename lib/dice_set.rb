60b8278b3d530af56d5d25ca80872b3654bcd425
Timothy Barmann <tim@mojotech.com>
2016-02-24 11:27:41 -0500

    Update dice_set.rb

--- lib/dice_set.rb
+++ lib/dice_set.rb
@@ -5,7 +5,7 @@ class DiceSet
     if number_of_dice > 0
 	    @values = []
 	    number_of_dice.times do
-	    	@values.push(Random.rand(1...6))
+	    	@values.push(Random.rand(1..6))
 	 	end
   	end
   end
